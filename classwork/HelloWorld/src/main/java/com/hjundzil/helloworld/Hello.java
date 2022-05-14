1f1480392c6b2145d1814286797d6ecc0053365c
halju <hjundzil@knights.ucf.edu>
2022-05-13 21:17:34 -0400

    Hello World in NetBeans

---
+++ classwork/HelloWorld/src/main/java/com/hjundzil/helloworld/Hello.java
@@ -0,0 +1,14 @@
+/**
+ *@author Haley Jundzil
+ *email: hjundzil@gmail.com
+ *date: 5/13/22
+ *purpose: My first Hello World program in an IDE
+*/
+
+package com.hjundzil.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Hal!");
+    }
+}
